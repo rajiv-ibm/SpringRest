e0d993ac5354e719b5a1ed9b5e72ea11833927db
rajiv-ibm <hrrajiva@gmail.com>
2016-12-21 20:01:51 +0530

    mongo example
    impl of spring data repo of mongo flavour

---
+++ src/main/java/mongo/UserRepository.java
@@ -0,0 +1,9 @@
+package mongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String>{
+	
+	public User findByName(String name);
+
+}
